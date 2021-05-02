29f5e62e23052efd49068673704a89802748e6a6
seb776 <sebastien.maire@epitech.eu>
2021-05-03 00:29:00 +0200

    fixed cors

--- CyberNixHighScores/CyberNixHighScores/Startup.cs
+++ CyberNixHighScores/CyberNixHighScores/Startup.cs
@@ -25,12 +25,22 @@ namespace CyberNixHighScores
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAnyOrigin",
+                                  builder =>
+                                  {
+                                      builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                                  });
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors("AllowAnyOrigin");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
